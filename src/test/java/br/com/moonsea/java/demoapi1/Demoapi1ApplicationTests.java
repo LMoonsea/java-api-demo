56103a4feb40ff0185c5f54833c263d62dbf186f
LMoonsea <ladymoonsea@gmail.com>
2023-06-12 19:39:04 -0300

    Initial commit

---
+++ src/test/java/br/com/moonsea/java/demoapi1/Demoapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moonsea.java.demoapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
